f0783d2ff31bc601a78252d1ce70432689a3b050
IgnatikVodichka <ignat136@gmail.com>
2021-05-01 03:29:58 +0300

    Update Lesson_2.py

---
+++ Algorithms/Lesson_2.py
@@ -0,0 +1,11 @@
+# Finding sum and multiplication of three digit number of which it consists:
+x = int(input('Please Enter a three digit number: '))
+a = x // 100
+b = x % 100 // 10
+c = x % 10
+
+sum = a + b + c
+mult = a * b * c
+
+print(f'Sum of numbers of your number = {sum}')
+print(f'Multiplication of numbers of your number = {mult}')
