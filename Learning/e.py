0de834bb5ff13af25b2b6457f1af03c6df9fa049
IgnatikVodichka <ignat136@gmail.com>
2021-05-01 16:17:40 +0300

    Create e.py

---
+++ Learning/e.py
@@ -0,0 +1,6 @@
+num1 = float((input('Enter First Number:')))
+num2 = float((input('Enter Second Number:')))
+num3 = float((input('Enter Third Number:')))
+
+print('You have entered following numbers', num1, num2, num3)
+print('Average of these numbers will be', (num1+num2+num3) / 3)
