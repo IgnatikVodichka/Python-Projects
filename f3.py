352a8de7e8e6fc6578894db6038004adce2b4397
IgnatikVodichka <ignat136@gmail.com>
2021-05-02 17:40:37 +0300

    Delete f3.py

--- f3.py
+++
@@ -1,7 +0,0 @@
-print('Please enter two numbers')
-x = int(input('Please enter number x: '))
-y = int(input('Please enter number y: '))
-if x != 0 and y != 0:
-    print(f'Division of x / y = {x / y}')
-else:
-    print('x / y = undefined')
