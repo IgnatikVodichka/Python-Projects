42d1fd90b845d498f9bca2d578b0e8bd70300153
IgnatikVodichka <ignat136@gmail.com>
2021-05-02 17:40:31 +0300

    Create Lesson_4.1.py

---
+++ Algorithms/Lesson_4.1.py
@@ -0,0 +1,11 @@
+print('Hello please enter three numbers to know which one is the greatest number')
+a = int(input('Please enter a: '))
+b = int(input('Please enter b: '))
+c = int(input('Please enter c: '))
+m = a
+if m < b:
+    m = b
+if m < c:
+    m = c
+
+print(f'Greatest number of three is {m}')
