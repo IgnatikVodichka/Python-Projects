9756e0ef97c41d51dd3d5e5b0988e2091c778279
IgnatikVodichka <ignat136@gmail.com>
2021-05-02 17:40:51 +0300

    Create f3.py

---
+++ Learning/f3.py
@@ -0,0 +1,7 @@
+print('Please enter two numbers')
+x = int(input('Please enter number x: '))
+y = int(input('Please enter number y: '))
+if x != 0 and y != 0:
+    print(f'Division of x / y = {x / y}')
+else:
+    print('x / y = undefined')
