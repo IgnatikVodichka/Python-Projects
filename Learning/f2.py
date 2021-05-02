8ce4e478faf8ad4c485b76e3ac7053cf3fe14468
IgnatikVodichka <ignat136@gmail.com>
2021-05-02 17:40:48 +0300

    Update f2.py

--- Learning/f2.py
+++ Learning/f2.py
@@ -1,4 +1,4 @@
-a = input('Enter value: 1 or 2 or 3 or 0 ')
+a = int(input('Enter value: 1 or 2 or 3 or 0 '))
 b = a == 1 or a == 2 or a == 3
 x = True if b else False  # shortened elif function.
 print(f'Ooopsyy...looks like its {x}')
