affdb5e142fe7740fad4f289e01c82ad5f3dc078
IgnatikVodichka <ignat136@gmail.com>
2021-05-02 17:40:20 +0300

    Update Lesson_1.py

--- Algorithms/Lesson_1.py
+++ Algorithms/Lesson_1.py
@@ -2,8 +2,8 @@ print('Enter two numbers x and y')
 x = int(input('Please enter x: '))
 y = int(input('Please enter y: '))
 
-if y != 0:
+if x != 0 and y != 0:
     c = x / y
-    print('c =', c)
+    print(f'c = {c}')
 else:
     print('c = undefined')
