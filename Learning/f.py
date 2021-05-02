206d9dec111628dc4872bc01fc16f3b859d25a40
IgnatikVodichka <ignat136@gmail.com>
2021-05-02 16:03:03 +0300

    Create f.py

---
+++ Learning/f.py
@@ -0,0 +1,9 @@
+a = input('Enter value: 1 or 2 or 3 or 0 ')
+if a == '0':
+    print(f'You have entered zero {a}')
+if a == '1' or a == '2' or a == '3':
+    print(f'You have entered wrong number {a}')
+    if 5 == 5:
+        print('5 = 5')
+else:
+    print('Error 404')
