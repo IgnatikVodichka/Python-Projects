0eeae108b5301e2ac22d0420f8af5a1f5ff6c71a
IgnatikVodichka <ignat136@gmail.com>
2021-05-02 17:40:42 +0300

    Update b.py

--- Learning/b.py
+++ Learning/b.py
@@ -14,7 +14,7 @@ print("x / y =", x / y)  # division
 # Modulus. remeinder of division (returns what number is left after division)
 print("x % y =", x % y)
 
-# floor division. (returns the greatest integer.integer is either less than or equal to the normal division result )
+# floor division. (returns the greatest integer, integer is either less than or equal to the normal division result )
 print("x // y =", x // y)
 
 # Exponantiation. Returns first number to the power of second one.
@@ -23,7 +23,7 @@ print("x ** y =", x ** y)
 
 # BINARY SYSTEM OPERATIONS AND OTHER
 
-# bin function takes any number and interprets it in binary system as zeros and ones. (starts wuth '0b')
+# bin function takes any number and interprets it in binary system as zeros and ones. (starts with '0b')
 print("x =", x, "=", bin(x))
 # hexodecimal numeral system 16 numbers (starts with '0x')
 print("15 =", hex(15))
@@ -48,7 +48,10 @@ print('~y=', ~y)
 # Signed right shift / Shift left by pushing zeros in from the right and let the leftmost bits fall off / basicly devides number by 2
 print('x>>1', bin(x >> 1))
 
-# Zero fill left shift / Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off / basicly multiplies number by 2
+# Zero fill left shift
+# Shift right by pushing copies of the leftmost bit in from the left,
+# and let the rightmost bits fall off
+# basicly multiplies number by 2
 print('y<<1', bin(y << 1))
 
 print('x>>1', x >> 1)  # same as upstairs but in decimal number system
