1a08ba65250a8928926d5487fb32dc0031e7b9e4
IgnatikVodichka <ignat136@gmail.com>
2021-05-23 14:59:17 +0300

    Add new lessons and homework

---
+++ Learning-Python/MainFunctions/3.1.List_And_Tuple_Functions.py
@@ -0,0 +1,36 @@
+user_list = [1, 2, 3, 4, 5]
+user_list[0] = 2
+print(user_list)
+user_list.append(9)  # adds an element to the end of the list
+print(user_list)
+
+user_list.extend([0, 1, 34, 82])  # extends a list by some number of elements
+print(user_list)
+
+user_list.insert(0, 5)  # inserts element to list in the 0 position(index)
+print(user_list)
+
+print(user_list.index(0))  # calls for element's '0' index
+print(user_list.count(0))  # counts how many time element '0' is in the list
+
+user_list.reverse()
+print(user_list)  # makes list to be backwards
+
+user_list.remove(82)  # deletes element 82 from the list
+print(user_list)
+
+# deletes element by it's index i.e '3', so element '9' will be deleted
+user_list.pop(3)
+print(user_list)
+user_list.clear()  # completeley erases the list to make it empty
+print(user_list)
+
+user_list = [0, 4, 8, 2, 1, 3, 10, 5]
+print(user_list)
+
+user_list.sort()  # sorts the list from smallest number to greatest
+print(user_list)
+
+# sorts the list from greatest to smallest numbers
+user_list.sort(reverse=True)
+print(user_list)
---
+++ Learning-Python/MainFunctions/3.2.Homework.py
@@ -0,0 +1,9 @@
+user_list = []
+num_ele = int(
+    input('Please eneter number of elements to be inside the list: '))
+i = 0
+while i in range(num_ele):
+    user_list.append(input('Enter element to be inside the list:  '))
+    i += 1
+
+print(user_list)
